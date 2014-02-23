06ae9ee4a371a9c80b41ee5a87f9e57ff283c21c
Daniel Warren Bond <dbond2@gmu.edu>
2014-02-23 16:15:15 -0500

    set up executable for gunicorn

---
+++ dbondcc/dbondcc.py
@@ -0,0 +1,10 @@
+#!flask/bin/python
+from website import website
+
+# website.run(debug = True) ## This is for debugging use only.
+
+if __name__ == 'main': ## This is for execution via gunicorn
+    website.run(debug=True)
+
+# gunicorn command
+# gunicorn dbondcc:website -b 127.0.0.1:8001
--- dbondcc/run.py
+++ dbondcc/run.py
@@ -1,2 +1,7 @@
+#!flask/bin/python
 from website import website
-website.run(debug = True)
+
+website.run(debug = True) ## This is for debugging use only.
+
+# execution command
+# python run.py
