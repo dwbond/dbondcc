fda17513d32e3d84f175b66b8a97af0f1a55f733
Daniel Warren Bond <dbond2@gmu.edu>
2014-03-07 21:17:45 -0500

    I shouldn't even be alive

--- dbondcc/website/__init__.py
+++ dbondcc/website/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from flask.ext.gravatar import Gravatar
+#from flask.ext.gravatar import Gravatar
 
 website = Flask(__name__)
 from website import views
 
-gravatar = Gravatar(website,
-                    size=100,
-                    rating='g',
-                    default='retro',
-                    force_default=False,
-                    use_ssl=False,
-                    base_url=None)
+#gravatar = Gravatar(website,
+#                    size=100,
+#                    rating='g',
+#                    default='retro',
+#                    force_default=False,
+#                    use_ssl=False,
+#                    base_url=None)
